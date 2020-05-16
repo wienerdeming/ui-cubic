4ef4ebbd6a1dc7c5daa52fd349bf622cf65e69f6
DalerLeo <dalerleo@gmail.com>
2020-05-17 04:22:06 +0500

    dependecies updated

--- demo/components/Buttons/Buttons.js
+++ demo/components/Buttons/Buttons.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, BorderedButton, SecondaryButton, DangerButton } from 'Button'
 import SortingTitle from 'SortingTitle'
+import {Button as ButtonES} from '../../../dist/index.es'
 import { Title, Description } from '../Utils'
 const buttonStyle = {
   marginRight: '20px'
@@ -14,6 +15,7 @@ const Buttons = props => {
 
       <div>
 
+        <ButtonES style={buttonStyle}>Main button</ButtonES>
         <Button style={buttonStyle}>Main button</Button>
         <BorderedButton style={buttonStyle}>Bordered button</BorderedButton>
         <SecondaryButton style={buttonStyle}>Secondary button</SecondaryButton>
